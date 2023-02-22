3a85c004f941bd82372e284fca32195650378c33
Richard de Rozario <richard.derozario@gmail.com>
2023-02-23 06:16:23 +1100

    corrected pack URL

--- src/pack.pl
+++ src/pack.pl
@@ -3,4 +3,4 @@ title('Library of prolog predicates to access the GPT API').
 version('0.1.00').
 author('Richard de Rozario','richard.derozario@gmail.com').
 home('https://github.com/RdR1024/prolog2gpt').
-download('https://raw.githubusercontent.com/RdR1024/prolog2gpt/main/rel/prolog2gpt-0.1.00.zip').
+download('https://github.com/RdR1024/prolog2gpt/raw/main/rel/prolog2gpt-0.1.00.zip').
